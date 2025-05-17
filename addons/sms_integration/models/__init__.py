dfac6b82058edc699b7422bbdbab7e8f905f5da4
Mohammad Glimbaf <mohammad.gelimbaf@gmail.com>
2025-05-18 00:14:22 +0330

    Remove odoo directory from version control

--- addons/sms_integration/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import providers
-from . import short_codes
-from . import sms_composer
-from . import sms_sms
-from . import mail_message #1
-from . import message_thread #2
-from . import mail_notification
-
-
